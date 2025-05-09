89a7330d36ad3121156fc768cef4b40423f0f40b
Makisuo <minetoaster12@gmail.com>
2025-05-09 23:30:18 +0200

    feat: add cli v2

---
+++ src/commands/add.command.ts
@@ -0,0 +1,40 @@
+import { Args, Command, Options } from "@effect/cli";
+import { Effect } from "effect";
+import { REGISTRY_URL } from "~/consts";
+
+import { Command as RawCommand } from "@effect/platform";
+
+export const componentNames = Args.text({ name: "componentNames" }).pipe(
+	Args.repeated,
+);
+
+export const componentType = Options.choice("type", ["ui", "block"]).pipe(
+	Options.withDefault("ui"),
+);
+
+export const addCommand = Command.make(
+	"add",
+	{ componentNames, componentType },
+	(config) =>
+		Effect.gen(function* () {
+			const componentPaths = yield* Effect.forEach(
+				config.componentNames,
+				(name) =>
+					Effect.succeed(
+						`${REGISTRY_URL}/r/${config.componentType}-${name}.json`,
+					),
+			);
+
+			const exitCode = yield* RawCommand.make(
+				"bunx",
+				"shadcn",
+				"add",
+				...componentPaths,
+			).pipe(
+				RawCommand.stdin("inherit"),
+				RawCommand.stdout("inherit"),
+				RawCommand.stderr("inherit"),
+				RawCommand.exitCode,
+			);
+		}),
+).pipe(Command.withDescription("Adds UI components or blocks to your project."));
---
+++ src/commands/init.command.ts
@@ -0,0 +1,35 @@
+import { Command } from "@effect/cli";
+import { Effect } from "effect";
+
+import { Command as RawCommand } from "@effect/platform";
+import { REGISTRY_URL } from "~/consts";
+
+import { BunCommandExecutor } from "@effect/platform-bun";
+
+const shadcnInit = RawCommand.make(
+	"bunx",
+	"shadcn",
+	"init",
+	`${REGISTRY_URL}/r/default.json`,
+).pipe(
+	RawCommand.stdin("inherit"),
+	RawCommand.stdout("inherit"),
+	RawCommand.stderr("inherit"),
+);
+
+export const initCommand = Command.make("init", {}, () =>
+	Effect.gen(function* () {
+		const results = yield* RawCommand.exitCode(shadcnInit);
+		// const test = yield* Effect.tryPromise(
+		// 	async () =>
+		// 		await execa(
+		// 			"bunx",
+		// 			["shadcn", "init", `${REGISTRY_URL}/r/default.json`],
+		// 			{
+		// 				stdio: "inherit",
+		// 				reject: false,
+		// 			},
+		// 		),
+		// );
+	}).pipe(Effect.scoped, Effect.provide(BunCommandExecutor.layer)),
+).pipe(Command.withDescription("Initializes the project and sets up necessary configurations."));
