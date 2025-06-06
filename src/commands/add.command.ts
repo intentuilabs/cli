58d23e3fc33b5fed13ccd5342bb6096635c6576f
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-06-06 23:09:24 +0700

    release beta-4

--- src/commands/add.command.ts
+++ src/commands/add.command.ts
@@ -1,7 +1,4 @@
 import { Args, Command, Options } from "@effect/cli"
-import { Console, Effect, Schema, pipe } from "effect"
-import { REGISTRY_URL } from "~/consts"
-
 import {
   HttpClient,
   HttpClientRequest,
@@ -9,20 +6,22 @@ import {
   Command as RawCommand,
 } from "@effect/platform"
 import chalk from "chalk"
+import { Console, Effect, Schema, pipe } from "effect"
+import { REGISTRY_URL } from "~/consts"
 import { Component } from "~/schema/component"
 
 export const componentNames = Args.text({ name: "componentNames" }).pipe(Args.repeated)
 
 export const isBlock = Options.boolean("b")
 export const isStyle = Options.boolean("s")
-
 export const allComponents = Options.boolean("all").pipe(
   Options.withAlias("a"),
   Options.withDefault(false),
 )
-
-export const overwrite = Options.boolean("overwrite").pipe(Options.withDefault(false))
-
+export const overwrite = Options.boolean("overwrite").pipe(
+  Options.withAlias("o"),
+  Options.withDefault(false),
+)
 export const componentType = Options.choice("type", ["ui", "block", "style"]).pipe(
   Options.withAlias("t"),
   Options.withDefault("ui"),
@@ -35,20 +34,7 @@ export const addCommand = Command.make(
     Effect.gen(function* () {
       const type = isBlock ? "block" : isStyle ? "style" : componentType
 
-      const componentPaths = yield* pipe(
-        Effect.forEach(componentNames, (name) =>
-          Effect.succeed(
-            name.startsWith("http")
-              ? name
-              : name.startsWith("-")
-                ? null
-                : `${REGISTRY_URL}/r/${type}-${name}.json`
-          ),
-        ),
-        Effect.map((list) => list.filter((url): url is string => url !== null))
-      )
-
-      if (!allComponents && componentPaths.length === 0) {
+      if (!allComponents && componentNames.length === 0) {
         yield* Console.log(chalk.red("No components selected"))
         yield* Console.log(chalk.red("Please select a component or use --all"))
         return
@@ -56,22 +42,41 @@ export const addCommand = Command.make(
 
       if (allComponents) {
         const client = yield* HttpClient.HttpClient.pipe()
-
         const response = yield* HttpClientRequest.get("https://intentui.com/r/index.json").pipe(
           client.execute,
           Effect.flatMap(HttpClientResponse.schemaBodyJson(Schema.Array(Component))),
         )
-
         const components = response.map((c) => `${REGISTRY_URL}/r/${c.name}.json`)
-        componentPaths.push(...components)
+        const args = ["add"]
+        if (overwrite) args.push("--overwrite")
+        args.push(...components)
+        return yield* pipe(
+          RawCommand.make("shadcnClone", ...args).pipe(
+            RawCommand.stdin("inherit"),
+            RawCommand.stdout("inherit"),
+            RawCommand.stderr("inherit"),
+            RawCommand.exitCode,
+          ),
+        )
       }
+
+      const manualOverwrite =
+        overwrite || componentNames.includes("--overwrite") || componentNames.includes("-o")
+
+      const cleanedComponentNames = componentNames.filter(
+        (name) => name !== "--overwrite" && name !== "-o",
+      )
+
+      const componentPaths = yield* pipe(
+        Effect.forEach(cleanedComponentNames, (name) =>
+          Effect.succeed(name.startsWith("http") ? name : `${REGISTRY_URL}/r/${type}-${name}.json`),
+        ),
+      )
 
       const args = ["add"]
-
-      if (overwrite) {
+      if (manualOverwrite) {
         args.push("--overwrite")
       }
-
       args.push(...componentPaths)
 
       return yield* pipe(
